a7930b9cc3c39b17373d862ddd5690dff9b11c1c
zkfly1991 <nuszkfly@hotmail.com>
2017-02-20 17:02:56 +0800

    New version of the loop function, add all profiles

--- PlayerLoop.py
+++ PlayerLoop.py
@@ -1,50 +1,71 @@
 from urllib2 import urlopen
 from bs4 import BeautifulSoup
 import csv
+import html5lib
 from string import ascii_lowercase
 
+#website prefix
+prefix = "http://www.basketball-reference.com"
 #For store all webpages
 webpages = {}
 #For store all players info
 players= {}
 #loop a to z to get all players info (No player name start with x):
 for c in range(ord('a'), ord('w')+1)+ range(ord('y'), ord('z')+1):
-    # webpages[c]= 'http://www.basketball-reference.com/players/' + chr(c) +"/"
-    # print webpages[c]
-    #
     webpages[c] = urlopen('http://www.basketball-reference.com/players/' + chr(c) +"/").read()
 
-    soup = BeautifulSoup(webpages[c])
+    soup = BeautifulSoup(webpages[c],'html5lib')
+    #find the player tables
     body = soup.find("table",{"id":"players"}).find("tbody").findAll("tr")
 
     for element in body:
+
         players[element.a.get_text()]={}
 
-    prefix = "http://www.basketball-reference.com"
+        #Scrap each individual players' first year
+        name = element.find('th',{"class":"left", "data-stat":"player"}).a.get_text()
+        players[element.a.get_text()]["name"] = name
 
-    for element in body:
         #Scrap each individual players' URL link
         players[element.a.get_text()]["link"] = prefix + element.a["href"]
 
-    for element in body:
-        #Scrap each individual players' last year
+        #Scrap each individual players' first year
         year_min = element.find('td',{"class":"right", "data-stat":"year_min"}).get_text()
         players[element.a.get_text()]["firstyear"] = year_min
 
-    for element in body:
         #Scrap each individual players' last year
         year_max = element.find('td',{"class":"right", "data-stat":"year_max"}).get_text()
         players[element.a.get_text()]["lastyear"] = year_max
 
+        #Scrap each individual players' position
+        pos = element.find('td',{"class":"center ","data-stat":"pos"}).get_text()
+        players[element.a.get_text()]["position"] = pos
+
+        #Scrap each individual players' height
+        height = element.find('td',{"class":"right ", "data-stat":"height"}).get_text()
+        players[element.a.get_text()]["height"] = height
+
+        #Scrap each individual players' weight
+        weight = element.find('td',{"class":"right ", "data-stat":"weight"}).get_text()
+        players[element.a.get_text()]["weight"] = weight
+
+        #Scrap each individual players' colleage name
+        try:
+            college = element.find('td',{"class":"left ", "data-stat":"college_name"}).a.get_text()
+        except AttributeError:
+            players[element.a.get_text()]["college"] = ''
+            continue #skip to the next loop.
+        else:
+            players[element.a.get_text()]["college"] = college
+
+
 for item in players.keys():
     if int(players[item]["lastyear"])> 2004 & int(players[item]["firstyear"])< 2015:
-        print item + ": " + "\n\t" + "link: " + players[item]["link"] + "\n\t" + "last year: " + players[item]["lastyear"]
+        print players[item]["name"] + ": " + "\n\t" + "link: " + players[item]["link"] + "\n\t" + "last year: " + players[item]["lastyear"] + "\n\t" + "college: " + players[item]["college"]
 
 
 
 # out = open('PlayerList.csv','w')
-#
-#
 # #print header
 # for j in header.find_all("th"):
 # 	out.write(j.get_text()+",")
