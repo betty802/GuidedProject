7a7a2407393c17e534161efd8ddca1d0742d5629
zkfly1991 <nuszkfly@hotmail.com>
2017-02-21 21:09:47 +0800

    scrap all players' profile, per game and salary

--- PlayerLoop.py
+++ PlayerLoop.py
@@ -4,64 +4,128 @@ import csv
 import html5lib
 from string import ascii_lowercase
 
+import pandas as pd
+import numpy as np
+
 #website prefix
 prefix = "http://www.basketball-reference.com"
-#For store all webpages
-webpages = {}
-#For store all players info
-players= {}
-#loop a to z to get all players info (No player name start with x):
+#For store all webpages for players last name from a to z
+profile_webpages = {}
+
+#Use 'A' page (all players last name start with A) to get the hearder list for the table
+webpage_a = urlopen('http://www.basketball-reference.com/players/a/').read()
+soup_a = BeautifulSoup(webpage_a,'html5lib')
+profile_header_list = []
+#Fetch headers
+profile_header = soup_a.find("table",{"id":"players"}).find("thead").find("tr")
+for j in profile_header.find_all("th"):
+    profile_header_list.append(j.get_text())
+#Insert a player_id and player_url columns to the first
+profile_header_list.insert(0,'player_id')
+profile_header_list.insert(1,'player_url')
+print profile_header_list
+
+#Temp list to store profile table
+t_profile = []
+#loop a to z to get all players info expect x since no players' last name start with x:
 for c in range(ord('a'), ord('w')+1)+ range(ord('y'), ord('z')+1):
-    webpages[c] = urlopen('http://www.basketball-reference.com/players/' + chr(c) +"/").read()
+    profile_webpages[c] = urlopen('http://www.basketball-reference.com/players/' + chr(c) +"/").read()
+    profile_soup = BeautifulSoup(profile_webpages[c],'html5lib')
 
-    soup = BeautifulSoup(webpages[c],'html5lib')
-    #find the player tables
-    body = soup.find("table",{"id":"players"}).find("tbody").findAll("tr")
+    #find the player profile for each player
+    body = profile_soup.find("table",{"id":"players"}).find("tbody").findAll("tr")
+    for i in body:
+        #temp list to store player info
+        s = []
+        #player_id:
+        s.append(i.find('th', {"class":"left ", "data-stat":"player"})["data-append-csv"])
+        #player_url:
+        s.append(prefix + i.find('th', {"class":"left ", "data-stat":"player"}).a["href"])
+        #player name:
+        s.append(i.find('th', {"class":"left ", "data-stat":"player"}).a.get_text())
+        #All other player info:
+        for k in i.find_all("td"):
+            s.append(k.get_text())
+        print s
+        t_profile.append(s)
 
-    for element in body:
+profile_table_all = pd.DataFrame(t_profile,columns=profile_header_list)
+#Take only the players who have played during 2005/2006 to 2014/2015 seasons.
+profile_table_all[['From','To']] = profile_table_all[['From','To']].apply(pd.to_numeric)
+profile_table = profile_table_all[(profile_table_all.From<2016)&(profile_table_all.To>2005)]
+print profile_table
 
-        players[element.a.get_text()]={}
-
-        #Scrap each individual players' first year
-        name = element.find('th',{"class":"left", "data-stat":"player"}).a.get_text()
-        players[element.a.get_text()]["name"] = name
-
-        #Scrap each individual players' URL link
-        players[element.a.get_text()]["link"] = prefix + element.a["href"]
-
-        #Scrap each individual players' first year
-        year_min = element.find('td',{"class":"right", "data-stat":"year_min"}).get_text()
-        players[element.a.get_text()]["firstyear"] = year_min
-
-        #Scrap each individual players' last year
-        year_max = element.find('td',{"class":"right", "data-stat":"year_max"}).get_text()
-        players[element.a.get_text()]["lastyear"] = year_max
-
-        #Scrap each individual players' position
-        pos = element.find('td',{"class":"center ","data-stat":"pos"}).get_text()
-        players[element.a.get_text()]["position"] = pos
-
-        #Scrap each individual players' height
-        height = element.find('td',{"class":"right ", "data-stat":"height"}).get_text()
-        players[element.a.get_text()]["height"] = height
-
-        #Scrap each individual players' weight
-        weight = element.find('td',{"class":"right ", "data-stat":"weight"}).get_text()
-        players[element.a.get_text()]["weight"] = weight
-
-        #Scrap each individual players' colleage name
-        try:
-            college = element.find('td',{"class":"left ", "data-stat":"college_name"}).a.get_text()
-        except AttributeError:
-            players[element.a.get_text()]["college"] = ''
-            continue #skip to the next loop.
-        else:
-            players[element.a.get_text()]["college"] = college
+profile_csv_path = 'player_profile.csv'
+profile_table.to_csv(profile_csv_path)
 
 
-for item in players.keys():
-    if int(players[item]["lastyear"])> 2004 & int(players[item]["firstyear"])< 2015:
-        print players[item]["name"] + ": " + "\n\t" + "link: " + players[item]["link"] + "\n\t" + "last year: " + players[item]["lastyear"] + "\n\t" + "college: " + players[item]["college"]
+
+        # players[element.a.get_text()]={}
+        #
+        # #Scrap each individual players' first year
+        # name = element.find('th',{"class":"left", "data-stat":"player"}).a.get_text()
+        # players[element.a.get_text()]["name"] = name
+        #
+        # #Scrap each individual players' URL link
+        # players[element.a.get_text()]["link"] = prefix + element.a["href"]
+        #
+        # #Scrap each individual players' first year
+        # year_min = element.find('td',{"class":"right", "data-stat":"year_min"}).get_text()
+        # players[element.a.get_text()]["firstyear"] = year_min
+        #
+        # #Scrap each individual players' last year
+        # year_max = element.find('td',{"class":"right", "data-stat":"year_max"}).get_text()
+        # players[element.a.get_text()]["lastyear"] = year_max
+        #
+        # #Scrap each individual players' position
+        # pos = element.find('td',{"class":"center ","data-stat":"pos"}).get_text()
+        # players[element.a.get_text()]["position"] = pos
+        #
+        # #Scrap each individual players' height
+        # height = element.find('td',{"class":"right ", "data-stat":"height"}).get_text()
+        # players[element.a.get_text()]["height"] = height
+        #
+        # #Scrap each individual players' weight
+        # weight = element.find('td',{"class":"right ", "data-stat":"weight"}).get_text()
+        # players[element.a.get_text()]["weight"] = weight
+        #
+        # #Scrap each individual players' colleage name
+        # try:
+        #     college = element.find('td',{"class":"left ", "data-stat":"college_name"}).a.get_text()
+        # except AttributeError:
+        #     players[element.a.get_text()]["college"] = ''
+        #     continue #skip to the next loop.
+        # else:
+        #     players[element.a.get_text()]["college"] = college
+
+
+# for item in players.keys():
+#     if int(players[item]["lastyear"])> 2004 & int(players[item]["firstyear"])< 2015:
+#         print players[item]["name"] + ": " + "\n\t" + "link: " + players[item]["link"] + "\n\t" + "last year: " + players[item]["lastyear"] + "\n\t" + "college: " + players[item]["college"]
+
+#
+# for item in players.keys():
+#
+# webpage = urlopen('http://www.basketball-reference.com/players/a/allenra02.html').read()
+# #print "<!--  \n    <div class=\"section_content\" id=\"div_transactions\"> \n    </div>\n\n-->  "
+# #webpage =  "<!--  \n    <div class=\"section_content\" id=\"div_transactions\"> \n    </div>\n\n-->  "
+#
+# #re.sub(r"^\s+","",webpage,flags=re.MULTILINE)
+#
+# soup = BeautifulSoup(re.sub("<!--|-->","",webpage),'html5lib')
+#
+# #re.sub(r"<!--\s+\n\s+<div",r"\n<div",webpage,flags=re.MULTILINE)
+# #re.sub(r"/div>\s+-->",r"/div>\n",webpage,flags=re.MULTILINE)
+#
+# #print webpage
+#
+# #soup = BeautifulSoup(webpage, "html.parser")
+#
+# print soup.prettify()
+#
+# header = soup.find("table",{"class":"suppress_glossary sortable stats_table","id":"all_salaries","data-cols-to-freeze":2 }).find("thead")
+# table = soup.find("table",{"class":"suppress_glossary sortable stats_table","id":"all_salaries","data-cols-to-freeze":2}).find("tbody").findAll("tr")
+#
 
 
 
--- player_stats.py
+++ player_stats.py
@@ -1,52 +1,138 @@
 from urllib2 import urlopen
 from bs4 import BeautifulSoup
-import re
 import csv
+import re
 import html5lib
+import pandas as pd
+import numpy as np
+import time
+import httplib
 
-#webpage passed from kaixiang
-webpage = urlopen('http://www.basketball-reference.com/players/a/allenra02.html').read()
-#print "<!--  \n    <div class=\"section_content\" id=\"div_transactions\"> \n    </div>\n\n-->  "
-#webpage =  "<!--  \n    <div class=\"section_content\" id=\"div_transactions\"> \n    </div>\n\n-->  "
+player_profile = pd.DataFrame(pd.read_csv("player_profile.csv"))
+player_urls = player_profile['player_url'].tolist()
 
-#re.sub(r"^\s+","",webpage,flags=re.MULTILINE)
+# Creating objects
+# Temp list to append header and content later
+pergame_header_list = []
+t_pergame = []
+# Temp list to append header and content later
+salary_header_list = []
+t_salary = []
 
-soup = BeautifulSoup(re.sub("<!--|-->","",webpage),'html5lib')
+time.sleep(0.5)
+# Take the first player page to fetch all headers for per game and salary tables:
+webpage = urlopen(player_urls[0]).read()
+soup = BeautifulSoup(re.sub("<!--|-->", "", webpage), 'html5lib')
+# Fetch headers for pergame table
+pergame_header = soup.find("table", {"id": "per_game"}).find("thead").find("tr")
+for j in pergame_header.find_all("th"):
+    pergame_header_list.append(j.get_text())
+#Insert a player id column in the header
+pergame_header_list.insert(0,'player_id')
+print pergame_header_list
+# Fetch headers for salary table
+salary_header = soup.find("table", {"class": "suppress_glossary sortable stats_table", "id": "all_salaries"}).find(
+    "thead").find("tr")
+for j in salary_header.find_all("th"):
+    salary_header_list.append(j.get_text())
+#Insert a player id column in the header
+salary_header_list.insert(0,'player_id')
+print salary_header_list
 
-#re.sub(r"<!--\s+\n\s+<div",r"\n<div",webpage,flags=re.MULTILINE)
-#re.sub(r"/div>\s+-->",r"/div>\n",webpage,flags=re.MULTILINE)
 
-#print webpage
+for i in player_urls:
+    try:
+        time.sleep(1)
+        webpage = urlopen(i).read()
+        soup = BeautifulSoup(re.sub("<!--|-->", "", webpage), 'html5lib')
+        # find the player tables
+        pergame_soup = soup.find("table", {"id": "per_game"}).find("tbody").findAll("tr", {"class": "full_table"})
+        #Strip out player id from the gamelog url
+        player_id=(pergame_soup[0].find('th', {"class": "left ", "data-stat": "season"}).a["href"]).split('/')[3]
 
-#soup = BeautifulSoup(webpage, "html.parser")
+        for i in pergame_soup:
+            s = []
+            s.append(player_id)
+            s.append(i.find('th', {"class": "left ", "data-stat": "season"}).a.get_text())
+            for k in i.find_all("td"):
+                s.append(k.get_text())
+            print s
+            t_pergame.append(s)
 
-print soup.prettify()
+        # salary_table = salary_table.append(salary_header_list, ignore_index=True)
 
-header = soup.find("table",{"class":"suppress_glossary sortable stats_table","id":"all_salaries","data-cols-to-freeze":2 }).find("thead")
-table = soup.find("table",{"class":"suppress_glossary sortable stats_table","id":"all_salaries","data-cols-to-freeze":2}).find("tbody").findAll("tr")
+        salary_soup = soup.find("table", {"class": "suppress_glossary sortable stats_table", "id": "all_salaries"}).find(
+            "tbody").findAll("tr")
+        for i in salary_soup:
+            s = []
+            s.append(player_id)
+            s.append(i.find("th").get_text())
+            for k in i.find_all("td"):
+                s.append(k.get_text())
+            print s
+            t_salary.append(s)
+    except:
+        AttributeError
+        httplib.IncompleteRead
+        continue
 
-#table1 = soup.find("table",{"id":"per_game"}).findAll("tr",{"class":"light_text partial_table"})
-#file name
+pergame_table = pd.DataFrame(t_pergame, columns=pergame_header_list)
+print pergame_table
+salary_table = pd.DataFrame(t_salary, columns=salary_header_list)
+print salary_table
 
-out = open('rayallen.csv', 'w')
+pergame_csv_path = 'player_pergame.csv'
+salary_csv_path = 'player_salary.csv'
+pergame_table.to_csv(pergame_csv_path)
+salary_table.to_csv(salary_csv_path)
 
-# print header
-for j in header.find_all("th"):
-    out.write(j.get_text() + ",")
-out.write("\n")
-# print table
-for i in table:
-    d = i.find("th");
-    out.write(d.get_text() + ",")
-    for j in i.find_all("td"):
-        out.write(j.get_text() + ",")
-    out.write("\n")
+# df = pd.DataFrame(np.random.randn(8, 4), columns=['A','B','C','D'])
+#
+# for element in salary:
+#     player_salary[element.a.get_text()] = {}
+#
+#     # Scrap each individual player_salary' first year
+#     name = element.find('th', {"class": "left", "data-stat": "player"}).a.get_text()
+#     player_salary[element.a.get_text()]["name"] = name
+#
+# soup = BeautifulSoup(re.sub("<!--|-->", "", webpage), 'html5lib')
+#
+# # re.sub(r"<!--\s+\n\s+<div",r"\n<div",webpage,flags=re.MULTILINE)
+# # re.sub(r"/div>\s+-->",r"/div>\n",webpage,flags=re.MULTILINE)
+#
+# # print webpage
+#
+# # soup = BeautifulSoup(webpage, "html.parser")
+#
+# print soup.prettify()
+#
+# header = soup.find("table", {"class": "suppress_glossary sortable stats_table", "id": "all_salaries",
+#                              "data-cols-to-freeze": 2}).find("thead")
+# table = soup.find("table", {"class": "suppress_glossary sortable stats_table", "id": "all_salaries",
+#                             "data-cols-to-freeze": 2}).find("tbody").findAll("tr")
 
-#for i in table1:
+# table1 = soup.find("table",{"id":"per_game"}).findAll("tr",{"class":"light_text partial_table"})
+# file name
+
+# out = open('rayallen.csv', 'w')
+#
+# # print header
+# for j in header.find_all("th"):
+#     out.write(j.get_text() + ",")
+# out.write("\n")
+# # print table
+# for i in table:
+#     d = i.find("th");
+#     out.write(d.get_text() + ",")
+#     for j in i.find_all("td"):
+#         out.write(j.get_text() + ",")
+#     out.write("\n")
+
+# for i in table1:
 #    d = i.find("th");
- #   out.write(d.get_text() + ",")
- #   for j in i.find_all("td"):
- #       out.write(j.get_text() + ",")
-  #  out.write("\n")
-out.close()
+#   out.write(d.get_text() + ",")
+#   for j in i.find_all("td"):
+#       out.write(j.get_text() + ",")
+#  out.write("\n")
+# out.close()
 print 'Done!'
